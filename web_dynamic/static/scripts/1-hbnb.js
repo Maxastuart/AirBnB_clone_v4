cb589a7042ad710035caeebc86a40bdb992dd985
akeemseymens <812@holbertonschool.com>
2020-02-17 20:22:25 +0000

    added the methond in 2.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -10,4 +10,12 @@ $(function () {
       }
       $('div.amenities h4').html(Object.values(chosen_amenities).join(', ') || '&nbsp;');
     });
-});
+
+  $.getJson("http://0.0.0.0:5001/api/v1/status/", function (data) {
+    if (data === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+   }); 
+  });
