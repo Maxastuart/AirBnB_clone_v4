753373727fb033ef5b95e61592833d4bc518d81f
akeemseymens <812@holbertonschool.com>
2020-02-17 22:22:45 +0000

    for task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,22 @@
+$(function () {
+  const chosen_amenities = {};
+
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        chosen_amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete chosen_amenities[$(this).attr('data-id')];
+      }
+      $('div.amenities h4').html(Object.values(chosen_amenities).join(', ') || '&nbsp;');
+    });
+  
+      $.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
+        if (data === 'OK') {
+          $('DIV#api_status').addClass('available');
+        } else {
+          $('DIV#api_status').removeClass('available');
+        }
+      });
+     
+});
