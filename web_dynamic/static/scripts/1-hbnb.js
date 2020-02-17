5efc37936c5fe99bb79a859c8223cd8afd3d186e
Max Stuart <maxastuart@gmail.com>
2020-02-17 19:37:35 +0000

    fix spacing & formatting

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,10 +4,10 @@ $(function () {
   $('div.amenities li input').change(
     function () {
       if ($(this).is(':checked')) {
-        chosen_amenities[($(this).attr('data-id'))] = $(this).attr('data-name');
+        chosen_amenities[$(this).attr('data-id')] = $(this).attr('data-name');
       } else {
         delete chosen_amenities[$(this).attr('data-id')];
       }
       $('div.amenities h4').html(Object.values(chosen_amenities).join(', ') || '&nbsp;');
-   });
+    });
 });
