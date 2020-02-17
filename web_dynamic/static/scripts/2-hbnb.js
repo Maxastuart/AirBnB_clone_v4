8cdcaa900d3b3d4c9e304edf57ebe009692e78bb
Max Stuart <maxastuart@gmail.com>
2020-02-17 19:45:20 +0000

    blank 2.html & 2.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,13 @@
+$(function () {
+  const chosen_amenities = {};
+
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        chosen_amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete chosen_amenities[$(this).attr('data-id')];
+      }
+      $('div.amenities h4').html(Object.values(chosen_amenities).join(', ') || '&nbsp;');
+    });
+});
