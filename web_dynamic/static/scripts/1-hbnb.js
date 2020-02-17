5c9668434100d25c4866fa9943d7bf99aaae6877
akeemseymens <812@holbertonschool.com>
2020-02-17 08:52:03 +0000

    changed logic

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-  const amenityList = {};
-  $('INPUT:checkbox').change(function () {
-    if ($(this).is(':checked')) {
-      const key = $(this).attr('data-id');
-      const value = $(this).attr('data-name');
-      amenityList[key] = value;
-    }
-    if (!$(this).is(':checked')) {
-      delete amenityList[$(this).attr('data-id')];
-    }
-  });
+$(function () {
+  const chosen_amenties = {};
+
+  $('div.amenties li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        chosen_amenties[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete chosen_amenties[$(this).attr('data-id')];
+      }
+      $('div.amenties h4').html(Object.values(chosen_amenties).join(', ') || '&nbsp');
+   });
 });
