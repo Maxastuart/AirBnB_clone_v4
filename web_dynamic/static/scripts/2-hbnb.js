29b436333bee64a10449f6eb08613d8be7aa7f32
Max Stuart <maxastuart@gmail.com>
2020-02-17 21:23:39 +0000

    swapped 2 and 1 and got the red status working

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,14 @@
 $(function () {
   const chosen_amenities = {};
 
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
   $('div.amenities li input').change(
     function () {
       if ($(this).is(':checked')) {
