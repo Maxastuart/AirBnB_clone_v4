c397659c98557e7304f98ab69bb34ca577144efb
Max Stuart <maxastuart@gmail.com>
2020-02-17 07:08:33 +0000

    Fixed 2 (files name 1-_)

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  const amenity_list = {};
-  $('INPUT:checkbox').change(function() {
+$(document).ready(function () {
+  const amenityList = {};
+  $('INPUT:checkbox').change(function () {
     if ($(this).is(':checked')) {
-      let key = $(this).attr('data-id')
-      let value = $(this).attr('data-name')
-      amenity_list[key] = value
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      amenityList[key] = value;
     }
     if (!$(this).is(':checked')) {
-      delete amenity_list[$(this).attr('data-id')]
+      delete amenityList[$(this).attr('data-id')];
     }
-  })
-})
+  });
+});
